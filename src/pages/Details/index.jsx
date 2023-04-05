922a11fc8ea2f26e87114496b19d2a3e8ee751d3
Thiago Elifas <tthiagoelifas@gmail.com>
2023-04-05 16:00:08 -0300

    install styled-components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,10 @@
-import "./styles.css"
+import { Container } from './styles';
 
 export function Details(){
   return(
-    <>
+    <Container>
       <h1>Hello Word!</h1>  
       <span>Rodigo Gonçalves</span>
-    </>
+    </Container>
   )
 }
