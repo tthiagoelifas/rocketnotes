e7a41b60693655f0a9151d1b754db58ae16fde4a
Thiago Elifas <tthiagoelifas@gmail.com>
2023-04-06 17:16:00 -0300

    creating props for button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from "./styles";
 
-export function Button(){
+export function Button({title, loading = false, ...rest}){
 
   return(
-    <Container type="button">
-      Cliqui aqui
+    <Container 
+    type="button"
+    disabled = {loading}
+    {...rest}
+    >
+      { loading ? "Carregando..." :  title} 
     </Container>
   )
 };
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -5,10 +5,12 @@ import { Button } from '../../components/Button';
 export function Details(){
   return(
     <Container>
-      <h1>Hello Word!</h1>  
+      <h1>Hello World!</h1>  
       <span>Rodigo Gonçalves</span>
 
-      <Button />
+      <Button title = "Entrar" loading/>
+      <Button title = "Cadastrar"/>
+      <Button title = "Voltar"/>
     </Container>
   )
 }
