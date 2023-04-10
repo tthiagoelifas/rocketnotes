3e54573ba77cedcc5818ea24551b7e42e03d53ea
Thiago Elifas <tthiagoelifas@gmail.com>
2023-04-10 09:13:37 -0300

    creating header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -5,11 +5,6 @@ import { Button } from '../../components/Button';
 export function Details(){
   return(
     <Container>
-      <h1>Hello World!</h1>  
-      <span>Rodigo Gonçalves</span>
-
-      <Button title = "Entrar" loading/>
-      <Button title = "Cadastrar"/>
       <Button title = "Voltar"/>
     </Container>
   )
