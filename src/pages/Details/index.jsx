bd0662944fb144f2dba81d14d4183dac0302ee78
Thiago Elifas <tthiagoelifas@gmail.com>
2023-04-10 15:46:53 -0300

    Finihed Detail page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,27 +1,41 @@
-import { Container, Links } from './styles';
+import { Container, Links, Content } from './styles';
 
-import { Header } from '../../components/Header';
-import { Button } from '../../components/Button';
-import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { ButtonText } from "../../components/ButtonText"
 
 export function Details(){
   return(
     <Container>
       <Header />
 
-      <Section title = "Links úteis">
-        <Links> 
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-        </Links>
-      </Section>
-      <Section title = "Marcadores"> 
-        <Tag title = "express"/>
-        <Tag title = "nodejs"/>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title= "Excluir nota" />
 
-      <Button title = "Voltar"/>
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam sunt cumque voluptates ea placeat minus veritatis nobis necessitatibus eaque consequuntur blanditiis saepe voluptate eligendi doloribus a, repudiandae autem tempora sapiente?</p>
+
+          <Section title = "Links úteis">
+            <Links> 
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+            </Links>
+          </Section>
+          <Section title = "Marcadores"> 
+            <Tag title = "express"/>
+            <Tag title = "nodejs"/>
+          </Section>
+
+          <Button title = "Voltar"/>
+
+        </Content>
+      </main>
     </Container>
   )
 }
