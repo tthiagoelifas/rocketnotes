893055e8e695df70b07e94810046db76c11dc07c
Thiago Elifas <tthiagoelifas@gmail.com>
2023-04-11 11:25:16 -0300

    add note component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,9 @@
 import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from "../../components/Header";
+import { Note } from "../../components/Note";
 import { Input } from "../../components/Input";
+import { Section } from "../../components/Section";
 import {ButtonText} from "../../components/ButtonText";
 
 export function Home(){
@@ -22,7 +24,15 @@ export function Home(){
         <Input placeholder= "Pesquisar pelo título" />
       </Search>
       <Content>
-
+        <Section title="Minhas notas">
+          <Note data={{
+            title: "React",
+            tags: [
+            {id: '1',name:'react'},
+            {id: '2',name:'rocketseat'}
+            ]
+          }} />
+        </Section>
       </Content>
       <NewNote>
         <FiPlus/>
