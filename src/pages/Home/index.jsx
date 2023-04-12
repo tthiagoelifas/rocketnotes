522de7ee9a623d9e783066734055d0a63cba631a
Thiago Elifas <tthiagoelifas@gmail.com>
2023-04-12 16:40:51 -0300

    end of stage 09

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { FiPlus, FiSearch } from 'react-icons/fi';
+import { FiPlus } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from "../../components/Header";
 import { Note } from "../../components/Note";
@@ -34,7 +34,7 @@ export function Home(){
           }} />
         </Section>
       </Content>
-      <NewNote>
+      <NewNote to="/new">
         <FiPlus/>
          Criar nota
       </NewNote>
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -5,3 +5,13 @@ import { Home } from "../pages/Home";
 import { New } from "../pages/New";
 import { Profile } from "../pages/Profile";
 
+export function AppRoutes(){
+  return(
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+      <Route path="/new" element={ <New /> } />
+      <Route path="/profile" element={ <Profile /> } />
+      <Route path="/details/:id" element={ <Details /> } />
+    </Routes>
+  )
+}
