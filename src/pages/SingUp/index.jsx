7e3f4aee6b86d4d03c350823d25fcc798df25563
Thiago Elifas <tthiagoelifas@gmail.com>
2023-04-12 15:45:19 -0300

    creating routes

--- src/pages/SingUp/index.jsx
+++ src/pages/SingUp/index.jsx
@@ -1,7 +1,7 @@
-import { FiMail, FiLock, FiUser } from "react-icons/fi"
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
 
-import { Input } from "../../components/Input"
-import { Button } from "../../components/Button"
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 import { Container, Form, Background } from "./styles";
  
---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,7 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Details } from "../pages/Details";
+import { Home } from "../pages/Home";
+import { New } from "../pages/New";
+import { Profile } from "../pages/Profile";
+
