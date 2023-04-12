02b177e4df57b57ca4d471b43abb52972acc17bc
Thiago Elifas <tthiagoelifas@gmail.com>
2023-04-12 09:29:58 -0300

    add Profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,7 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
 export function Profile(){
   return(
@@ -13,6 +13,19 @@ export function Profile(){
       </header>
 
       <Form>
+        <Avatar>
+          <img 
+            src="https://github.com/tthiagoelifas.png"
+            alt="Foto do usuário" 
+          />
+          <label htmlFor="avatar">
+            <FiCamera/>
+            <input
+             id="avatar"
+             type="file"
+            />
+          </label>
+        </Avatar>
         <Input
           placeholder = "Nome"
           type = "text"
