8896816b99e32afd99d2b25f94dd29fa7ede1689
Thiago Elifas <tthiagoelifas@gmail.com>
2023-04-12 14:46:25 -0300

    add NoteItem component

--- src/components/Textarea/styles.js
+++ src/components/Textarea/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.textarea`
   width: 100%;
-  height: 150%;
+  height: 150px;
 
   background-color: ${({theme}) => theme.COLORS.BACKGROUND_900};
   color: ${({theme}) => theme.COLORS.WHITE};
